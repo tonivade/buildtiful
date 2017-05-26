8bb06a8187e2062626261e48880d1a2eb5a811ba
Antonio Muñoz <antonio.munoz@rumbo.com>
2017-05-26 13:27:52 +0200

    yaml parsed

--- build.sbt
+++ build.sbt
@@ -4,4 +4,5 @@ version := "0.1.0-SNAPSHOT"
 
 scalaVersion := "2.12.1"
 
-libraryDependencies += "net.jcazevedo" %% "moultingyaml" % "0.4.0"
+libraryDependencies += "io.circe" %% "circe-yaml" % "0.5.0"
+libraryDependencies += "io.circe" %% "circe-generic" % "0.8.0"
--- src/main/scala/com/github/tonivade/buildtiful/Buildtiful.scala
+++ src/main/scala/com/github/tonivade/buildtiful/Buildtiful.scala
@@ -1,18 +1,28 @@
 package com.github.tonivade.buildtiful
 
-import net.jcazevedo.moultingyaml._
-import net.jcazevedo.moultingyaml.DefaultYamlProtocol._
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.yaml._
+import scala.io.Source
 
-object Buildtiful {
-  def main(args: Array[String]) {
-  }
-}
-
-case class Build (project: Project, plugins: Seq[String], repositories: Seq[Repository], depencencies: Dependencies, build: Seq[String])
+case class Build (project: Project, plugins: Seq[String], repositories: Seq[Repository], dependencies: Dependencies, build: Seq[String])
 
 case class Project (groupId: String, artifactId: String, version: String)
 
 case class Repository (id: String, url: String)
 
 case class Dependencies (compile: Seq[String], test: Seq[String])
+
+object Buildtiful {
+  def main(args: Array[String]) {
+    import io.circe.yaml.parser
+    
+    val yamlString = Source.fromFile(args(0)).mkString
+    
+    val build = parser.parse(yamlString).fold(x => x, _.as[Build])
+    
+    println(build)
+  }
+}
+
 
