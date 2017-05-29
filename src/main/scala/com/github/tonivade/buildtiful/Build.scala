378d6a3d820ac3cc09f17891f9bb22fb96c46b22
Antonio Muñoz <antonio.munoz@rumbo.com>
2017-05-29 09:18:34 +0200

    sources splitted in two, main and test

--- src/main/scala/com/github/tonivade/buildtiful/Build.scala
+++ src/main/scala/com/github/tonivade/buildtiful/Build.scala
@@ -1,6 +1,6 @@
 package com.github.tonivade.buildtiful
 
-case class Build(project: Project, source: Seq[String], plugins: Seq[String], 
+case class Build(project: Project, sources: Sources, plugins: Seq[String], 
                  repositories: Seq[Repository], dependencies: Dependencies, build: Seq[String])
 
 case class Project(groupId: String, artifactId: String, version: String)
@@ -8,3 +8,6 @@ case class Project(groupId: String, artifactId: String, version: String)
 case class Repository(id: String, url: String)
 
 case class Dependencies(compile: Seq[String], test: Seq[String])
+
+case class Sources(main: Seq[String], test: Seq[String])
+
