e494680a4cae79518f21df65f75bab0d05a82610
Antonio Muñoz <antonio.munoz@rumbo.com>
2017-05-31 17:02:43 +0200

    implementing compiler using Javac ant task

--- src/main/scala/Compiler.scala
+++ src/main/scala/Compiler.scala
@@ -1,11 +1,39 @@
 package com.github.tonivade.buildtiful
 
+import org.apache.tools.ant.Task
+import org.apache.tools.ant.taskdefs.Javac
+import org.apache.tools.ant.types.Path
+import org.apache.tools.ant.{Project => AntProject}
+import java.io.File
+
 trait Compiler {
   def compile(build: Build) : Unit
 }
 
 class DefaultCompiler extends Compiler {
+  val project : AntProject = {
+    val project = new AntProject()
+    project.setBaseDir(new File("target/buildtiful"));
+    project.init() 
+    project
+  }
+  
   def compile(build: Build) {
     println("compile")
+    
+    javac(build).execute()
+  }
+  
+  def javac(build: Build): Task = {
+    val javac = new Javac()
+    javac.setProject(project)
+    build.sources.main.map(src => new Path(project, src)).map(javac.setSrcdir(_))
+    build.sources.test.map(src => new Path(project, src)).map(javac.setSrcdir(_))
+    
+    val classpath = new Path(project)
+    classpath.setPath("target/buildtiful/libs")
+    javac.setClasspath(classpath)
+    
+    javac
   }
 }
